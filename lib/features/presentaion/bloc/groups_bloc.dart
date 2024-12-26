76b433ac7ec8ab99e2803d79bcd6248de88d28c2
MariyaRazboynikova <mariya.razboynikova@gmail.com>
2024-12-26 10:43:27 +0500

    1

--- lib/features/presentaion/bloc/groups_bloc.dart
+++ lib/features/presentaion/bloc/groups_bloc.dart
@@ -1,15 +1,23 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:kpop_application/features/data/models/groups_model.dart';
+import 'package:kpop_application/features/domain/repository/groups_repository.dart';
 
 part 'groups_event.dart';
 part 'groups_state.dart';
 
 class GroupsBloc extends Bloc<GroupsEvent, GroupsState> {
-  GroupsBloc() : super(GroupsInitial());
+  final GroupsRepository groupsRepository;
 
-  @override
-  Stream<GroupsState> mapEventToState(GroupsEvent event) async* {
-    // TODO: implement mapEventToState
+  GroupsBloc(this.groupsRepository) : super(GroupsInitial()) {
+    on<FetchGroup>((event, emit) async {
+      emit(GroupsLoading());
+      try {
+        Groups groups = await groupsRepository.getAllGroups();
+        emit(GroupsLoaded(groups));
+      } catch (e) {
+        emit(GroupsError(e.toString()));
+      }
+    });
   }
 }
