1a9e076792b544a50111cb9ba4347cc203972c4f
MariyaRazboynikova <mariya.razboynikova@gmail.com>
2024-12-28 13:11:47 +0500

    export

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:kpop_application/core/themes/themes_provider.dart';
-import 'package:kpop_application/features/data/api/api_service.dart';
-import 'package:kpop_application/features/data/repository/group_repository_impl.dart';
-import 'package:kpop_application/features/domain/repository/groups_repository.dart';
-import 'package:kpop_application/features/presentaion/bloc/groups_bloc.dart';
-import 'package:kpop_application/features/presentaion/pages/groups_page.dart';
-import 'package:kpop_application/features/presentaion/pages/idols_page.dart';
-import 'package:kpop_application/features/presentaion/pages/intro_page.dart';
-import 'package:kpop_application/features/presentaion/pages/members_page.dart';
-import 'package:provider/provider.dart';
+import 'package:kpop_application/export.dart';
 
 void main() {
   runApp(
@@ -42,9 +31,8 @@ class MainApp extends StatelessWidget {
         '/intro_page': (context) => const IntroPage(),
         '/groups_page': (context) => const GroupsPage(),
         '/members_page': (context) {
-          final args =
-              ModalRoute.of(context)!.settings.arguments
-                  as Map<String, dynamic>;
+          final args = ModalRoute.of(context)!.settings.arguments
+              as Map<String, dynamic>;
           return MembersPage(
             group: args['group'],
             idols: args['idols'],
@@ -52,9 +40,8 @@ class MainApp extends StatelessWidget {
           );
         },
         '/idols_page': (context) {
-          final args =
-              ModalRoute.of(context)!.settings.arguments
-                  as Map<String, dynamic>;
+          final args = ModalRoute.of(context)!.settings.arguments
+              as Map<String, dynamic>;
           return IdolsPage(
             memberId: args['memberId'],
             idols: args['idols'],
